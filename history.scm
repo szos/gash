da388883888eac129e493288b582a70ad96a0b42
Nathan <szos@posteo.net>
2019-05-09 13:00:37 -0700

    Added default history rule

--- history.scm
+++ history.scm
@@ -10,13 +10,12 @@
 
 (define *history-rules* '())
 
-(set! *history-rules* (cons (lambda (txt)
-			      #t)
-			    *history-rules*))
+(define (define-history-rule function)
+  "adds a lamda to the list of lambdas to compute whether or not to add an item 
+to the history list. "
+  (set! *history-rules* (cons function *history-rules*)))
 
 (define (add-history-item text)
-  ;; (display "adding ") (display-color 2 text) (echo " to history")
-  ;; (echo "adding to history")
   (let ((list-of-t-f (map (lambda (el)
 			    (el text))
 			  *history-rules*)))
@@ -27,9 +26,6 @@
 	(if (not (member #f list-of-t-f))
 	    (begin (set! *history-count* (+ *history-count* 1))
 		   (set! *history-holder* (cons text *history-holder*))))))
-  ;; (set! *history-count* (+ *history-count* 1))
-  ;; (set! *history-holder* (cons text *history-holder*))
-  ;; (echo "added to history")
   (if (or (equal? *history-count* *max-history*)
 	  (> *history-count* *max-history*))
       (begin ;; (echo "in if true")
@@ -40,5 +36,9 @@
 (define (history-previous-entry . l)
   (if (null? l)
       (second *history-holder*)
-      (let ((contents (cdr *history-holder*)))
-	(list-ref contents (car l)))))
+      (let ((contents (cdr *history-holder*))
+	    (num (car l)))
+	(if (> num (length contents))
+	    ;; (echo-color 1 "")
+	    (throw 'out-of-range "Within history-previous-entry: attempted to reference history item that doesnt exist")
+	    (list-ref contents num)))))
